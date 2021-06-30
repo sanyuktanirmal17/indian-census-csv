078d7d04d514be620d47d5b8ff514c243fab76a8
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-07-01 00:51:58 +0530

    added: test case 1.3

--- src/main/java/service/CustomCsvException.java
+++ src/main/java/service/CustomCsvException.java
@@ -2,6 +2,8 @@ package service;
 
 public class CustomCsvException extends Exception{
 
+	public static final String ExceptionType = null;
+
 	public CustomCsvException(String message) {
 		super(message);
 	}
--- src/test/java/service/IndianCensusTest.java
+++ src/test/java/service/IndianCensusTest.java
@@ -11,21 +11,32 @@ public class IndianCensusTest {
 	public void setUp() {
 		census = new IndianCensus();
 	}
-	
-	int result = census.getSizeOfCsv("IndiaStateCensusData.csv");
-	Assert.assertEquals(29, result);
-}
 
-@Test
-public void given_IndianCensusCSVFile_WhenIncorrect_ThrowsCustomException() {
-	try {
-		@SuppressWarnings("unused")
-		int result = census.getSizeOfCsv("IndiaStateCensusdddata.csv");
-	}
-	catch(CustomCsvException e) {
-		Assert.assertEquals(CustomCsvException.ExceptionType.INCORRECT_FILE, e.type);
+	@Test
+	public void given_IndianCensusCSVFile_ReturnsNumberOfRecords() throws CustomCsvException {
+		int result = census.getSizeOfCsv("IndiaStateCensusData.csv");
+		Assert.assertEquals(29, result);
 	}
 
-}
+	@Test
+	public void given_IndianCensusCSVFile_WhenIncorrect_ThrowsCustomException() {
+		try {
+			@SuppressWarnings("unused")
+			int result = census.getSizeOfCsv("IndiaStateCensusdddata.csv");
+		} catch (CustomCsvException e) {
+			Assert.assertEquals(CustomCsvException.ExceptionType.INCORRECT_FILE, e.type);
+		}
+	}
 
+	@Test
+	public void given_IndianCensusCSVFile_WhenTypeIsIncorrect_ThrowsCustomException() {
+		try {
+			@SuppressWarnings("unused")
+			int result = census.getSizeOfCsv("IndiaStateCensusData.txt");
+		}
+
+		catch (CustomCsvException e) {
+			Assert.assertEquals(CustomCsvException.ExceptionType.INCORRECT_FILE, e.type);
+		}
+	}
 }
